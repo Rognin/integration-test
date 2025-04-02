cf931d8ac8610c8167b7587e53f30530dc094d10
GitHub Action <action@github.com>
2025-04-02 10:45:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/rognin/integrationtest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rognin/integrationtest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.rognin.integrationtest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rognin.integrationtest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
